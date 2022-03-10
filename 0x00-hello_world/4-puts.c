ea1cd9646b4f6cab2a5dd9df0b1e4b0c7ff096cb
stacie-kipruto <staciekipruto@gmail.com>
2022-03-10 12:19:27 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints your code
+ * Return: it will return 0 if properly executed, if not 0 it will return other..
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
