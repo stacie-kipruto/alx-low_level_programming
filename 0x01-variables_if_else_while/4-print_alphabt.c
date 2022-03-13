32947f72b3d62173d80e11118f2dec37e4911d19
stacie-kipruto <staciekipruto@gmail.com>
2022-03-12 18:12:38 -0800

    when i was having alphbt soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *  main -Program entry point
+ *
+ *  Retur: on success. Error code otherwise
+ */
+int main (void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
