9d3b5d7e2ee464afb196dd7c14f58940591693e7
stacie-kipruto <staciekipruto@gmail.com>
2022-03-12 18:33:53 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf('\n');
+
+	return (0);
+}
