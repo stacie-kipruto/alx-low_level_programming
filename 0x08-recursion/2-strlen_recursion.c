3945c04394fa3415d23e361ccf02419c843e60e9
stacie-kipruto <staciekipruto@gmail.com>
2022-03-30 16:13:11 -0700

    dreams feel real when you're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return length of a string
+ * @s: string to return length
+ * Return: length of string s
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
