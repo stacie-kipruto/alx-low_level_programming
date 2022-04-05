c8773083b18d83c634ec276405a9520bf9d22436
stacie-kipruto <staciekipruto@gmail.com>
2022-04-04 22:21:42 -0700

    included appropriate library

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/** 
+ * _strdup(): Function that returns a pointer to a new string,
+ * which is a duplicate of the string (str)
+ * @str: pointer to the string being duplicated.
+ *
+ * Return: pointer to the duplicated string on success
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+	else
+		return (strdup(str));
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <string.h>
 int _putchar(char c);
 
 char *create_array(unsigned int size, char c);
