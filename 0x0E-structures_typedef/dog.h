8fe4c64fa1081900d9e77f0fe95782f28a8706bd
stacie-kipruto <staciekipruto@gmail.com>
2022-04-12 13:17:42 -0700

    poppy header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * struct dog - struct to contain dog details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
