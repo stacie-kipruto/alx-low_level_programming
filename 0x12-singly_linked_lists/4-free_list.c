e41bea7a3d5ea23d6120ca461f3b6838b57950df
stacie-kipruto <staciekipruto@gmail.com>
2022-04-22 00:50:56 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * free_list - frees a list_t list
+  * @head: pointer to head node
+  *
+  * Return: none!!
+  */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		free(temp->str);
+		head = head->next;
+		free(temp);
+	}
+}
